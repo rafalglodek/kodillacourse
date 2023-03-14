b08e88a98fd226b3362635e4a8f855febb545f13
Rafal Glodek <rafal.glodek@interia.pl>
2023-03-14 04:48:44 +0000

    Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -31,3 +31,19 @@ class StreamMain {
                 .forEach(System.out::println);
     }
     }
+/*
+    package com.kodilla.stream;
+
+            import com.kodilla.stream.book.BookDirectory;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+    }
+}
+
+ */
