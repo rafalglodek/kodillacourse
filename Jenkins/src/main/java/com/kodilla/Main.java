f7b786e8c9ada55e5565a2d9148257e1bc2555c3
Rafal Glodek <rafal.glodek@interia.pl>
2024-11-21 18:39:09 +0100

    Jenkins

--- Jenkins/src/main/java/com/kodilla/Main.java
+++ Jenkins/src/main/java/com/kodilla/Main.java
@@ -2,6 +2,6 @@ package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello Jenkins");
     }
 }
